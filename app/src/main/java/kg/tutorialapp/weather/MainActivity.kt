27961896f0f358f6cf16bf55d5b558b145afeab9
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-03-31 00:58:04 +0600

    Changed text styles.

--- app/src/main/java/kg/tutorialapp/weather/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/weather/MainActivity.kt
@@ -2,11 +2,14 @@ package kg.tutorialapp.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        Maintext.text = getString(R.string.greeting)
+//        меняю текст на новый, обращаюсь по id TextView и достаю строку с новым текстом
 
     }
 }
