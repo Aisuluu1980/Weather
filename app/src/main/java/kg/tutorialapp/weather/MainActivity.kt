a167d1b3eafd3c619a962008c5ec4bd2a54324df
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-04-01 22:09:20 +0600

    Done lesson_24 TextView, EditText.

--- app/src/main/java/kg/tutorialapp/weather/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/weather/MainActivity.kt
@@ -1,15 +1,30 @@
 package kg.tutorialapp.weather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textview: TextView
+    lateinit var edittext: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        Maintext.text = getString(R.string.greeting)
-//        меняю текст на новый, обращаюсь по id TextView и достаю строку с новым текстом
 
+//        val newText = findViewById(R.id.textView) as TextView
+//        newText.text = getString(R.string.greeting)
+//меняю текст на новый, обращаюсь по id TextView и достаю строку с новым текстом
+
+//        textView.text = getString(R.string.greeting)
+//        этот метод не сработал не видит по id
+
+        textview = findViewById(R.id.textView) as TextView
+        textview.text = getString((R.string.greeting))
+        edittext = findViewById(R.id.editText) as EditText
+
+        edittext.setOnClickListener({textview.text = edittext.text})
     }
 }
